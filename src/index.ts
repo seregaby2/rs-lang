e935a730ce9c3159fd29c88cf97e224460f71aee
seregaby2 <serega.by@inbox.ru>
2022-02-02 00:18:49 +0300

    feat: create branch develop

---
+++ src/index.ts
@@ -0,0 +1,5 @@
+import './styles/styles.scss';
+import { App } from './ui/app/app';
+
+const app: App = new App();
+app.start();
---
+++ webpack.config.js
@@ -0,0 +1,68 @@
+const path = require('path');
+const CopyPlugin = require("copy-webpack-plugin");
+const {merge} = require('webpack-merge');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const {CleanWebpackPlugin} = require('clean-webpack-plugin');
+const ESLintPlugin = require('eslint-webpack-plugin');
+
+
+const baseConfig = {
+  entry: path.resolve(__dirname, './src/index.ts'),
+  mode: 'development',
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          "style-loader",
+          "css-loader",
+          "sass-loader",
+        ],
+      },
+      {
+        test: /\.(png|jpg|svg|gif|.mp3)$/i,
+        use: ['file-loader']
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+  },
+  output: {
+    filename: 'index.js',
+    path: path.resolve(__dirname, './dist'),
+  },
+  plugins: [
+    new ESLintPlugin({
+      extensions: '.ts'
+    }),
+    new CopyPlugin({
+      patterns: [
+        {from: path.resolve(__dirname, 'src/assets'), to: path.resolve(__dirname, 'dist/assets')},
+      ],
+    }),
+
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, './src/index.html'),
+      filename: 'index.html',
+      inject: false,
+    }),
+    new CleanWebpackPlugin(),
+  ],
+};
+
+module.exports = ({mode}) => {
+  const isProductionMode = mode === 'prod';
+  const envConfig = isProductionMode ? require('./webpack.prod.config') : require('./webpack.dev.config');
+
+  return merge(baseConfig, envConfig);
+};
