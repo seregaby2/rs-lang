b8956f277f796c1f869df7bdc3bc4aab4a1d0a52
Alhladkiy <Alhladkiy@gmail.com>
2022-02-02 21:12:46 +0300

    feat: add controller for audio game

---
+++ src/ui/audioGame/models/wordResponse.model.ts
@@ -0,0 +1,16 @@
+export interface WordResponseModel {
+  readonly id: string,
+  readonly group: number,
+  readonly page: number,
+  readonly word: string,
+  readonly image: string,
+  readonly audio: string,
+  readonly audioMeaning: string,
+  readonly audioExample: string,
+  readonly textMeaning: string,
+  readonly textExample: string,
+  readonly transcription: string,
+  readonly wordTranslate: string,
+  readonly textMeaningTranslate: string,
+  readonly textExampleTranslate: string,
+}
