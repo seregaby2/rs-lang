c35389c45582501fb4a679df1cc48cf9f27fb9fe
Runeci <kostenokd@mail.ru>
2022-02-03 23:21:39 +0300

    refactor: LANG-18 remove comments

--- src/ui/app/app.ts
+++ src/ui/app/app.ts
@@ -1,19 +1,7 @@
-// import { HeaderView } from '../common/header/components/header';
-// import { Footer } from '../common/footer/components/footer';
-// import { HomePage } from '../homePage/components/homePage';
 import { Router } from '../routing/components/router';
 import { PageTemplate } from '../common/pageTemplate/template';
-// import { TeamInfo } from '../homePage/components/teamInfo';
 
 export class App {
-  // private header: HeaderView = new HeaderView();
-  //
-  // private footer: Footer = new Footer();
-
-  // private homePage: HomePage = new HomePage();
-
-  // private team: TeamInfo = new TeamInfo();
-
   private router: Router = new Router();
 
   private pageTemplate: PageTemplate = new PageTemplate();
