b1084987e5f35edf551d11ad5c4267b03f5b49ea
Alhladkiy <Alhladkiy@gmail.com>
2022-02-06 11:35:52 +0300

    refactor: clear app

--- src/ui/app/app.ts
+++ src/ui/app/app.ts
@@ -1,12 +1,4 @@
-import { AudioGamePage } from '../audioGame/components/audioGamePage';
-
 export class App {
   public start(): void {
-    this.showAudioPage();
-  }
-
-  private showAudioPage(): void {
-    const audioPage = new AudioGamePage();
-    audioPage.draw();
   }
 }
