b7005590a465f686a5a093dfddb759db98fb720b
Runeci <kostenokd@mail.ru>
2022-02-14 22:01:03 +0300

    fix: LANG-25 fix audio play

--- src/ui/textbookPage/card/cardAudio.ts
+++ src/ui/textbookPage/card/cardAudio.ts
@@ -2,9 +2,23 @@ import { ControllerWords } from '../../common/controller/controllerWords';
 import { IWordsData } from '../../common/controller/model';
 
 export class CardAudio {
+  private audioWord: HTMLAudioElement | undefined;
+
+  private audioMeaning: HTMLAudioElement | undefined;
+
+  private audioExample: HTMLAudioElement | undefined;
+
   private controller: ControllerWords = new ControllerWords();
 
   public playCardAudio(group: number, page: number): void {
+    const pagination = document.querySelector('.textbook-pagination') as HTMLElement;
+    const textbookHeader = document.querySelector('.textbook-header') as HTMLElement;
+    const header = document.querySelector('.header') as HTMLElement;
+    [pagination, textbookHeader, header].forEach((el) => {
+      el.addEventListener('click', () => {
+        this.stopAudioSet();
+      });
+    });
     const audioBtns = document.querySelectorAll('.textbook-card-sound') as NodeListOf<HTMLDivElement>;
     audioBtns.forEach((btn, btnNumber) => {
       btn.addEventListener('click', () => {
@@ -26,24 +40,31 @@ export class CardAudio {
     meaning: IWordsData['audioMeaning'],
     example: IWordsData['audioExample'],
   ): void {
-    const audioWord = new Audio(`https://rs-lang-2022.herokuapp.com/${word}`);
-    const audioMeaning = new Audio(`https://rs-lang-2022.herokuapp.com/${meaning}`);
-    const audioExample = new Audio(`https://rs-lang-2022.herokuapp.com/${example}`);
+    this.audioWord = new Audio(`https://rs-lang-2022.herokuapp.com/${word}`);
+    this.audioMeaning = new Audio(`https://rs-lang-2022.herokuapp.com/${meaning}`);
+    this.audioExample = new Audio(`https://rs-lang-2022.herokuapp.com/${example}`);
 
     this.disableAudioButtons();
 
-    audioWord.play();
-    audioWord.addEventListener('ended', () => {
-      audioMeaning.play();
+    this.audioWord?.play();
+
+    this.audioWord.addEventListener('ended', () => {
+      this.audioMeaning?.play();
     });
 
-    audioMeaning.addEventListener('ended', () => {
-      audioExample.play();
+    this.audioMeaning.addEventListener('ended', () => {
+      this.audioExample?.play();
     });
 
-    audioExample.addEventListener('ended', () => {
+    this.audioExample.addEventListener('ended', () => {
       this.enableAudioButtons();
     });
+  }
+
+  private stopAudioSet(): void {
+    this.audioExample?.pause();
+    this.audioWord?.pause();
+    this.audioMeaning?.pause();
   }
 
   private disableAudioButtons(): void {
