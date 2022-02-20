d0a1ade149cef65f4d4b2869db35a31adec80757
Alhladkiy <Alhladkiy@gmail.com>
2022-02-20 10:22:27 +0300

    refactor: delete spaces

--- src/ui/audioGame/components/templateHtmlAudioGame.ts
+++ src/ui/audioGame/components/templateHtmlAudioGame.ts
@@ -1,21 +1,4 @@
 export class TemplateHtmlAudioGame {
-  get templateSettings(): string {
-    return `
-    <div class="main-container-settings-audio-game">
-      <div class="container-settings-audio-game-main-title">
-        <h3 class="main-title-setting-audio-game">Настойки игры</h2>
-      </div>
-      <div class="container-support-title-settings-game">
-        <h4 class="support-title-setting-audio-game">Выберите сложность игры</h4>
-      </div>
-      <div class="container-buttons-level-audio-game"></div>
-      <div class="container-buttons-settings-audio-game">
-        <button class="button-audio-game button-cancel-audio-game">Отмена</button>
-        <button class="button-audio-game button-start-audio-game">Старт</button>
-      </div>
-    </div>`;
-  }
-
   get baseTemplate(): string {
     return `
     <div class="main-wrapper-audio-game">
